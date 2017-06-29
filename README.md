a92a40a97874b710076f5a7648db50d250ea744a
Jesse Croxford <takeflightsc@gmail.com>
2017-06-29 00:06:18 -0600

    added /api/whoami route

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This is a node microservice project built to fulfill the FreeCodeCamp [Request Header Parser Microservice](https://www.freecodecamp.com/challenges/request-header-parser-microservice) project. 
 
 ##Usage
-in order to use this app vist -- url -- it will send back a JSON object that contains your ip address, preferred browser language, and operating system information
+in order to use this app vist [header-parser-jc.herokuapp.com/api/whoami](header-parser-jc.herokuapp.com/api/whoami). It will send back a JSON object that contains your ip address, preferred browser language, and operating system information
--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const path    = require('path')
+const express   = require('express')
+const useragent = require('express-useragent')
+const path      = require('path')
 
 const publicPath = path.join(__dirname + '/../public')
 const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.static(publicPath))
+app.use(useragent.express())
 
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip
+  const language = req.acceptsLanguages()
+  const software = `OS: ${req.useragent.os}, Browser: ${req.useragent.browser}`
 
+  res.json({ipaddress, language: language[0], software})
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
